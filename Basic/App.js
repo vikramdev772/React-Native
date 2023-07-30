d1931b62a80bf00e4ff3a63e1980f1456e71fc71
vikram <vikramdev772@gmail.com>
2023-07-30 23:04:51 +0530

    img

--- Basic/App.js
+++ Basic/App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Image, TouchableOpacity, Text } from 'react-native';
+// import ImagePicker from 'react-native-image-picker';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+const App = () => {
+  const [backgroundImage, setBackgroundImage] = useState(require('./assets/programmer.png'));
+
+  const selectImage = () => {
+    
+    setBackgroundImage(require('./assets/layers.png'));
+
+  };
+
+  const changeImage=()=>{
+    setBackgroundImage(require('./assets/programmer.png'));
+  }
+
+  return (   
+   <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center',backgroundColor:"black" }}>
+      <Image
+        source={backgroundImage}
+        style={{ width: 370, height: 370, position: 'absolute' }}
+      />
+      <TouchableOpacity onPress={selectImage} style={{ padding: 10, backgroundColor: 'rgba(255, 255, 255, 0.5)', borderRadius: 5,marginTop:300 }}>
+        <Text>Change Background</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={changeImage} style={{ padding: 10, backgroundColor: 'skyblue', borderRadius: 5,marginTop:100 }}>
+        <Text>Change Background</Text>
+      </TouchableOpacity>
     </View>
-  );
+  )
 }
+  
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+export default App;
